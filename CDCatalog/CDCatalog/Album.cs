59b759df0eea859f138a42d6b51a6a9c963d05df
Chris Ramos <chris.ramos@alaskaair.com>
2016-01-30 20:27:48 -0800

    Initial Commit
    Initial Commit

---
+++ CDCatalog/CDCatalog/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDCatalog
+{
+    class Album
+    {
+        string title;
+        int year;
+        double rating;
+        string artist;
+    }
+}
---
+++ CDCatalog/CDCatalog/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDCatalog
+{
+    class Song
+    {
+        string title;
+        string artist;
+        string album;
+        int trackNumber;
+        string genre;
+        int trackLength;
+        double rating;
+    }
+}
