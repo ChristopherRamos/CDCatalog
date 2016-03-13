b4f0303b489fe266b55afbf942f62a7e6835018a
Chris Ramos <chris.ramos@alaskaair.com>
2016-03-13 16:40:58 -0700

    3/13 changes
    3/13 changes

---
+++ CDCatalog/CDCatalog/Details.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDCatalog
+{
+    class Details
+    {
+        public string AssetType { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string AlbumTitle { get; set; }
+        public int TrackNumber { get; set; }
+        public string Genre { get; set; }
+        public int TrackLength { get; set; }
+        public int Rating { get; set; }
+    }
+}
