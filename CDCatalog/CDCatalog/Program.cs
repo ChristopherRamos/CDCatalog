a6f5853f1b045a70b87fccabebf560cd3474ae05
Chris Ramos <chris.ramos@alaskaair.com>
2016-03-16 10:30:04 -0700

    3/16 exception handling pt. 1
    3/16 exception handling pt. 1

--- CDCatalog/CDCatalog/Program.cs
+++ CDCatalog/CDCatalog/Program.cs
@@ -14,10 +14,16 @@ namespace CDCatalog
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message);
+            }
         }
 
     }
